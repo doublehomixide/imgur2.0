2c38c5066bcb4f7b0fa5a0795d79a6a890e76e4f
Double Homixide <doublehomixide@gmail.com>
2025-02-03 16:41:54 +0300

    feat: models for reg/login and some changes

--- models/models.go
+++ models/models.go
@@ -3,11 +3,22 @@ package models
 import "io"
 
 type User struct {
-	ID             int    `gorm:"primary_key;autoIncrement" json:"id"`
-	Username       string `gorm:"unique" json:"username"`
-	Email          string `gorm:"unique" json:"email"`
-	HashedPassword string `json:"hashed_password"`
-	Images         []Image
+	ID       int    `gorm:"primary_key;autoIncrement" json:"id"`
+	Username string `gorm:"unique" json:"username"`
+	Email    string `gorm:"unique" json:"email"`
+	Password string `json:"password"`
+	Images   []Image
+}
+
+type UserLogin struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type UserRegister struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
 }
 
 type Image struct {
